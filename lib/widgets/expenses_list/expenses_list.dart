197076579580214d98962e2fa9310060ace28e3d
Rajath <rajathganeshmt@gmail.com>
2024-02-24 12:28:55 +0530

    Initial commit

---
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,32 @@
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/expenses_list/expense_item.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
+
+  final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      //ListViews are always Scrollable but .builder build or create items only when they are visible or about to become visible
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withOpacity(0.05),
+          margin: Theme.of(context).cardTheme.margin,
+        ),
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(
+          expenses[index],
+        ),
+      ),
+    );
+  }
+}
